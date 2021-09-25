ba66f9704630e9ce667eb8dab4f2b2c50bbe6b0d
Lenovo <Lenovo@Vikas>
2021-09-25 13:32:33 +0530

    commit message

--- learining/src/test/java/Learning/learining/Demo.java
+++ learining/src/test/java/Learning/learining/Demo.java
@@ -1,12 +1,23 @@
 package Learning.learining;
 
-import org.testng.annotations.Test;
+import java.util.Stack;
 
 public class Demo {
-	
-	@Test
-	public void t1() {
-		System.out.println("\\s"+"a");
+
+
+	public static void main(String[] args) {
+
+
+		String str = "String s";
+		Stack<Character> rev = new Stack<Character>();
+		for(int i=0; i<str.length(); i++) 
+
+			rev.add(str.charAt(i));
+			int j= rev.size();
+		for(int i=0; i<j; i++)
+
+			System.out.print(rev.pop());
+
 	}
 
 }
